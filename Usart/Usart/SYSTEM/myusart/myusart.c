9502c2deaa1ae6ebc41916c26698813e104405c9
MrSu <you@example.com>
2022-09-19 16:44:35 +0800

    “将串口接收的数组修改为在省电赛上使用过的消息循环队列FIFO”

--- Usart/Usart/SYSTEM/myusart/myusart.c
+++ Usart/Usart/SYSTEM/myusart/myusart.c
@@ -16,24 +16,33 @@ int fgetc(FILE *f)
   return ch;
 }
 
-char RxBuffer[RXBUFFERSIZE];  //接收数据
+/***********************************************
+* @brief : 串口接收回调函数
+* @param : 串口端口
+* @return: 消息循环队列
+* @date  : 2022.9.19
+* @author: 刘骏帆
+************************************************/
+RX_Queue usart_rx_queue;	//接收循环队列
 uint8_t aRxBuffer;			  //接收中断缓冲
-uint8_t Usart_Rx_Cnt = 0;     //接收缓冲计数
-
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 {
-	HAL_GPIO_WritePin(LED_GPIO_Port,LED_Pin,GPIO_PIN_RESET);
-	if(Usart_Rx_Cnt > 200)  //溢出判断
+	/*======================串口信息进入消息队列=================*/
+	if((usart_rx_queue.rear+1)%MAX_QUEUE_LENGTH==usart_rx_queue.front)//判断是否队满
 	{
-		Usart_Rx_Cnt = 0;
-		memset(RxBuffer,0,sizeof(RxBuffer));
+		usart_rx_queue.front=(usart_rx_queue.front+1)%MAX_QUEUE_LENGTH;//队头加一
 	}
 	else
 	{
-		RxBuffer[Usart_Rx_Cnt++] = aRxBuffer;
-		//可以在此处增加自己的逻辑函数，但是最好处理串口接收到的数据最好放在外面，否则容易卡死
+		usart_rx_queue.length++;
 	}
-
+	//无论如何队列的队尾都需要进队的，接收到的信息不能被放弃
+	usart_rx_queue.USART_RX_BUF[usart_rx_queue.rear]=aRxBuffer;
+	usart_rx_queue.rear=(usart_rx_queue.rear+1)%MAX_QUEUE_LENGTH;
+	/*=========================================================*/
+	//在以下部分添加通讯帧是否结束的逻辑判断代码
+	
+	
 	//因为接收中断使用了一次即关闭，所以在最后加入这行代码即可实现无限使用
 	//发现数据发的过多时程序会进不去中断，怀疑是这里再次使能失败
 	while(HAL_UART_Receive_IT(&RECEVEIUART, (uint8_t *)&aRxBuffer, 1)==HAL_BUSY);  
--- Usart/Usart/SYSTEM/myusart/myusart.h
+++ Usart/Usart/SYSTEM/myusart/myusart.h
@@ -3,17 +3,17 @@
 
 #define RETAGETUART	huart1	 //重定向串口端口
 #define	RECEVEIUART	huart1	 //接收串口端口
-#define RXBUFFERSIZE  200     //最大接收字节数
+#define MAX_QUEUE_LENGTH  200     //最大接收字节数
 
 //接收消息队列结构体
 typedef struct RX_Queue
 {
-    uint8_t USART_RX_BUF[RXBUFFERSIZE];//数据域
+    uint8_t USART_RX_BUF[MAX_QUEUE_LENGTH];//数据域
     int front;//队头指针
     int rear;//队尾指针
     uint8_t length;//队列当前长度
 }RX_Queue;
 
-extern char RxBuffer[RXBUFFERSIZE];  //接收数据
-extern uint8_t aRxBuffer;			  //接收中断缓冲
-//extern uint8_t Uart1_Rx_Cnt;         //接收缓冲计数
+extern RX_Queue usart_rx_queue;	//接收循环队列
+extern uint8_t aRxBuffer;	   //接收中断缓冲
+
